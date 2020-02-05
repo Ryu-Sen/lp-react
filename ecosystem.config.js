f8516d366b03679ebf791f0561a91bfdfadf664a
List-Senda <r-senda@list.co.jp>
2020-02-05 14:54:51 +0900

    npm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'lp-reatc8082',
-      script: 'yarn',
+      name: 'lp-react8082',
+      script: 'npm',
       args: 'start',
       env: {
         "PORT": 8082,
         "NODE_ENV": "development",
-      },
+      }
     },
   ],
 };
