4ffd6f995d56213747dddfabfd2ca54c390d514e
Mavroian Florin Ionut <f-mavroian@listjp.local>
2020-02-06 18:22:27 +0900

    routing to fpage

--- src/components/App.js
+++ src/components/App.js
@@ -1,76 +1,18 @@
 import React from "react";
-import { Switch, Route, Redirect, Link } from "react-router-dom";
-import { Layout, Menu } from "antd";
-
-import About from "./About";
-import Home from "./Home";
-import Articles from "./Articles";
-import Article from "./Articles/Article";
-import NotFound from "./NotFound";
-
+import { Switch, Route } from "react-router-dom";
+import Index from "../f-page/index"
 import "./App.css";
-
-const { Header, Content } = Layout
 
 class App extends React.PureComponent {
   render() {
     return (
-      // <div className="App">
-      <Layout style={{ minHeight: '100vh' }}>
-        <Header style={{ background: '#fff', padding: 0 }} >
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['1']}
-            style={{ lineHeight: '64px' }}
-          >
-            <Menu.Item key="1">
-              <span>Home</span>
-              <Link to="/" />
-            </Menu.Item>
-            <Menu.Item key="2">
-              <span>Articles</span>
-              <Link to="/articles" />
-            </Menu.Item>
-            <Menu.Item key="3">
-              <span>About</span>
-              <Link to="/about" />
-            </Menu.Item>
-          </Menu>
-        </Header>
-
-        <Content className="App">
+      <div className="App">
           <Switch>
-            {/* If the current URL is /about, this route is rendered
-              while the rest are ignored */}
-            <Route path="/about">
-              <About />
-            </Route>
-
-            <Route path="/articles">
-              <Articles />
-            </Route>
-
-            <Route path="/article/:id">
-              <Article />
-            </Route>
-
-            {/* Redirect if no param passed */}
-            <Redirect from="/article/" to="/articles" />
-
-            <Route exact path="/">
-              <Home />
-            </Route>
-
-            {/* If none of the previous routes render anything,
-              this route acts as a fallback.*/}
-            <Route path="*">
-              <NotFound />
-            </Route>
+            <Route path="/fpage">
+              <Index/>
+           </Route>
           </Switch>
-        </Content>
-      </Layout>
-      // </div>
+       </div>
     );
   }
 }
