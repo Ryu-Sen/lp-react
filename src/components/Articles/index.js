1db2173a22dae2fc0e2c878d1dd31147b1c65a4c
Thomas Alunni <t-gradini@list.co.jp>
2020-02-06 16:45:59 +0900

    test time webhook

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -4,6 +4,6 @@ import "./Articles.css";
 
 export default class Articles extends React.PureComponent {
     render() {
-        return <h1>Articles page !</h1>;
+        return <h1>Articles</h1>;
     }
 }
