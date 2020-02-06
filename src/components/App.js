f1e4f013ba70525e4036ffcb1784bd744a93a8a0
Thomas Alunni <t-gradini@list.co.jp>
2020-02-06 14:29:49 +0900

    Routing layout menu/header example

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import { Layout } from "antd";
+import { Switch, Route, Redirect, Link } from "react-router-dom";
+import { Layout, Menu } from "antd";
 
 import About from "./About";
 import Home from "./Home";
@@ -10,41 +10,67 @@ import NotFound from "./NotFound";
 
 import "./App.css";
 
-// const { Header } = Layout
+const { Header, Content } = Layout
 
 class App extends React.PureComponent {
   render() {
     return (
-      <div className="App">
-        <Switch>
-          {/* If the current URL is /about, this route is rendered
+      // <div className="App">
+      <Layout style={{ minHeight: '100vh' }}>
+        <Header style={{ background: '#fff', padding: 0 }} >
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">
+              <span>Home</span>
+              <Link to="/" />
+            </Menu.Item>
+            <Menu.Item key="2">
+              <span>Articles</span>
+              <Link to="/articles" />
+            </Menu.Item>
+            <Menu.Item key="3">
+              <span>About</span>
+              <Link to="/about" />
+            </Menu.Item>
+          </Menu>
+        </Header>
+
+        <Content className="App">
+          <Switch>
+            {/* If the current URL is /about, this route is rendered
               while the rest are ignored */}
-          <Route path="/about">
-            <About />
-          </Route>
+            <Route path="/about">
+              <About />
+            </Route>
 
-          <Route path="/articles">
-            <Articles />
-          </Route>
+            <Route path="/articles">
+              <Articles />
+            </Route>
 
-          <Route path="/article/:iddayum">
-            <Article />
-          </Route>
+            <Route path="/article/:id">
+              <Article />
+            </Route>
 
-          {/* Redirect if no param passed */}
-          <Redirect from="/article/" to="/articles" />
+            {/* Redirect if no param passed */}
+            <Redirect from="/article/" to="/articles" />
 
-          <Route exact path="/">
-            <Home />
-          </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
 
-          {/* If none of the previous routes render anything,
+            {/* If none of the previous routes render anything,
               this route acts as a fallback.*/}
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </div>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Content>
+      </Layout>
+      // </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+
+import "./index.css";
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <Router>
