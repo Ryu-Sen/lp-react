7a58cba8f6e6b16d6b9f94232ad54eec4ea91364
Thomas Alunni <t-gradini@list.co.jp>
2020-02-06 10:54:50 +0900

    react-router w/o Next

---
+++ src/components/About/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./About.css";
+
+export default class About extends React.PureComponent {
+  render() {
+    return <h1>About page</h1>;
+  }
+}
---
+++ src/components/Home/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Home.css";
+
+export default class Home extends React.PureComponent {
+  render() {
+    return <h1>Home page</h1>;
+  }
+}
