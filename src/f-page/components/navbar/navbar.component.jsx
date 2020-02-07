1e8347e5baf823a0097569487e30f970c47cbdf6
Mavroian Florin Ionut <f-mavroian@listjp.local>
2020-02-07 11:57:14 +0900

    removed search and added syles

--- src/f-page/components/navbar/navbar.component.jsx
+++ src/f-page/components/navbar/navbar.component.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import "./navbar.styles.css"
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
+import NavDropdown from 'react-bootstrap/NavDropdown' 
  
 const Header = ()=>{
   return(
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="#home">
+      <Navbar.Brand className="logo-brand" href="#home">
       <img
         alt=""
         src="./logo-light.png"
@@ -22,7 +19,7 @@ const Header = ()=>{
     </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
+        <Nav className="nav-container">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#link">Link</Nav.Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
@@ -33,10 +30,6 @@ const Header = ()=>{
           <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
           </NavDropdown>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
       </Navbar.Collapse>
     </Navbar>
   )
