0cc4d57d15c3d02ec8b38fbb4fa86b2986afc93b
Mavroian Florin Ionut <f-mavroian@listjp.local>
2020-02-12 11:57:33 +0900

    refactored code

--- src/f-page/components/simulator/simulator.component.jsx
+++ src/f-page/components/simulator/simulator.component.jsx
@@ -9,44 +9,49 @@ class Simulator extends Component{
     super()
     this.state= {
       pieDataPoints:[],
-      lineDataPoint:[]
+      lineDataPoints:[]
     }
   }
-  componentDidMount(){
-    $('form :input').on('change',(e)=>{console.log(e.target)})
+  getPieValues = ()=> {
+    let outputValues = []
+    let totalPayment = "";
+    let loanAmount = ""; //maybe I need it somewhere else
 
-    let values = []
-    $('form :input').each(function(index) {
-      return values.push({y: 50,label:this.name})
-  });
-
-  let outputValues = []
-  let totalPayment = "";
-  let loanAmount = ""; //maybe I need it somewhere else
-
-  $("._37q431tzwiJw837-Dlr_8F").children().each(function(){
-    let text = $(this).text().split(":$")
-    if(text[0] === "Total Payment"){
-     totalPayment =  Number.parseFloat(text[1].replace(/,/g, ''));
-    }
+    $("._37q431tzwiJw837-Dlr_8F").children().each(function(){
+      let text = $(this).text().split(":$")
+      if(text[0] === "Total Payment"){
+      totalPayment =  Number.parseFloat(text[1].replace(/,/g, ''));
+      }
     
-    if(text[0] === "Loan Amount"){
-      loanAmount =  Number.parseFloat(text[1].replace(/,/g, ''));
-    }
-  })
+      if(text[0] === "Loan Amount"){
+        loanAmount =  Number.parseFloat(text[1].replace(/,/g, ''));
+      }
+    })
 
-  $("._37q431tzwiJw837-Dlr_8F").children().each(function(index){
+    $("._37q431tzwiJw837-Dlr_8F").children().each(function(index){
 
       let text = $(this).text().split(":$")
       
       if(text[0]!== "Loan Amount" && text[0]!== "Total Payment"){
         outputValues.push({y:(Number.parseInt(text[1].replace(/,/g, ''))/totalPayment * 100).toFixed(2),label:text[0],x:index})
       }
-      
+        
     })
-    
+    return outputValues
+  }
+  getLineValues = ()=> {
+   
+    let values = []
+    $('form :input').each(function(index) {
+      values.push({y: Number.parseFloat($(this).val()),label:this.name,x:index})
+    });
+    return values
+  }
+
+  componentDidMount(){
     this.setState((state)=>{
-      state.pieDataPoints = outputValues
+      state.pieDataPoints = this.getPieValues()
+      state.lineDataPoints = this.getLineValues()
       return state
     },(console.log(this.state))) 
     
@@ -72,7 +77,10 @@ class Simulator extends Component{
             <MortgageCalculator  id="simulator" style={simulatorStyles}/>
             </div>
             <div style={{"float":"right"}}>
-            <PieChart pieOptions={this.state.pieDataPoints}/>
+            <PieChart pieOptions ={this.state.pieDataPoints} text = {"Payment Brakedown"}/>
+
+            <br></br>
+            <LineChart lineOptions = { this.state.lineDataPoints}  text = {"Statistics"}/>
 
             </div>
            
