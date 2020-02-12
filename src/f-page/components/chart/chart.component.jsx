2850d448c30c63726ab53660d62d88c49aad1523
Mavroian Florin Ionut <f-mavroian@listjp.local>
2020-02-12 18:04:50 +0900

    testing animation on canvas js lib

--- src/f-page/components/chart/chart.component.jsx
+++ src/f-page/components/chart/chart.component.jsx
@@ -1,46 +1,56 @@
 import CanvasJSReact from '../../../assets/canvasjs.react.js';  
-import React from 'react'
+import React, { Component } from 'react'
 
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-export const  PieChart =({pieOptions, text})=>{
-const options = {
-  exportEnabled: true,
-  animationEnabled: true,
-  title: {
-    text: text
-  },
-  legend: {
-    horizontalAlign: "right", // "center" , "right"
-    verticalAlign: "center",  // "top" , "bottom"
-    fontSize: 15
-  },
-  data: [{
-    type: "doughnut",
-    startAngle: 75,
-    toolTipContent: "<b>{label}</b>: {y}%",
-    showInLegend: "true",
-    legendText: "{label}",
-    indexLabelFontSize: 16,
-    indexLabel: "{label} - {y}%",
-    dataPoints: pieOptions
-  }]
+export class  PieChart extends Component{
+ 
+componentDidUpdate(){
+  this.chart.render()
 }
-return (
-  <div style={{"width":"600px"}}>
-    <h1>Loan Simulation Chart</h1>
-      <CanvasJSChart options = {options} 
-      /* onRef={ref => this.chart = ref} */
-    />
-    {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-  </div>
-  );
+render(){
+  const  options = {
+    animationEnabled: true,
+    exportEnabled: true,
+    title: {
+      text: this.props.text
+    },
+    legend: {
+      horizontalAlign: "right", // "center" , "right"
+      verticalAlign: "center",  // "top" , "bottom"
+      fontSize: 15
+    },
+    data: [{
+      type: "doughnut",
+      startAngle: 75,
+      toolTipContent: "<b>{label}</b>: {y}%",
+      showInLegend: "true",
+      legendText: "{label}",
+      indexLabelFontSize: 16,
+      indexLabel: "{label} - {y}%",
+      dataPoints: this.props.pieOptions
+    }]
+  }
+  return (
+    <div style={{"width":"600px"}}>
+      <h1>Loan Simulation Chart</h1>
+        <CanvasJSChart options = {options} 
+         onRef={ref =>{
+           return  this.chart = ref
+         }}
+      />
+      {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+    </div>
+    );
+}
+
 }
 
 export const LineChart = ({lineOptions, text})=>{
   const options = {
     animationEnabled: true,
-    theme: "light2", // "light1", "light2", "dark1", "dark2"
+    animationDuration: 2000,
+   
     title:{
       text: text
     },
@@ -53,11 +63,12 @@ export const LineChart = ({lineOptions, text})=>{
       dataPoints:lineOptions
     }]
   }
+  
   return (
     <div style={{"width":"500px"}}>
-      <h1>Loan Simulation Chart</h1>
         <CanvasJSChart options = {options} 
-        /* onRef={ref => this.chart = ref} */
+        /* onRef={ref => this.chart = ref}*/
+       
       />
       {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
     </div>
