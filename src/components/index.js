d19051ba2528312b618bd0e8a5b46b08a051f7c0
Thomas Alunni <t-gradini@list.co.jp>
2020-02-13 18:22:33 +0900

    Just some esthetic changes

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from "./Footer";
 export { default as ListCarousel } from "./ListCarousel";
 export { default as Team } from "./Team";
 export { default as Layout } from "./Layout";
+export { default as Contact } from "./Contact";
