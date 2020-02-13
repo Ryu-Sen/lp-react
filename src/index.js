7cf49d4b53386e7e442b9920244c52cc1fbf74ec
Thomas Alunni <t-gradini@list.co.jp>
2020-02-13 13:17:59 +0900

    second test w/ Helmet w/ custom layout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./pages/App";
 import * as serviceWorker from "./serviceWorker";
+import { Layout } from "./components";
 
 import "./index.css";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Layout description="This is the Landing Page main route! You're free to choose where you want to go!">
+      <App />
+    </Layout>
   </Router>,
   document.getElementById("root")
 );
