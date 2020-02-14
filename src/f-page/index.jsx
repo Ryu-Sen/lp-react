434f6e1f90c806fa2dc39476b738a5bdc1f82071
Mavroian Florin Ionut <f-mavroian@listjp.local>
2020-02-14 18:13:13 +0900

    added carousel

--- src/f-page/index.jsx
+++ src/f-page/index.jsx
@@ -5,6 +5,8 @@ import Simulator from "./components/simulator/simulator.component";
 import ContactForm from "./components/contact/contact.component"
 import MapContainer from "./components/map/map.component"
 import {Footer}  from "./components/footer/footer.component";
+import MyCarousel from "./components/carousel/carousel.component"
+import "./index.styles.css"
 
 class Index extends React.PureComponent {
   render() {
@@ -14,11 +16,16 @@ class Index extends React.PureComponent {
           <Header/>
           <div className="content clearfix">
               <div className="container">
-              <Simulator/>
-              <MapContainer/>
-              <ContactForm/>
+                <Simulator/>
+                <MyCarousel/>
+                <div className="contact-us">
+               
+                  <MapContainer/>
+                  <ContactForm/>
+                </div>
               </div>
           </div>
+          
           <Footer/>
        </div>
     );
